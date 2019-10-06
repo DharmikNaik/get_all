f1c323e811e8c956fb7345db38faf0c253e0ccd3
dharmiknaik <dharmiknaik@hotmail.com>
2019-10-06 10:18:28 +0530

    collected static files

--- get_all/settings.py
+++ get_all/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'static/css'), )
 
 django_heroku.settings(locals())
